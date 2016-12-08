404eddfadfb2ed4feb6a34220d7ec76fa3e21a3c
Ian Calaça <ian.costa@acad.pucrs.br>
2016-12-08 03:46:17 -0200

    Update Publisher.java

--- src/Publisher.java
+++ src/Publisher.java
@@ -4,12 +4,12 @@ import org.ros.node.DefaultNodeMainExecutor;
 import java.net.URI;
 
 public class Publisher {
-	public static void main(String[] args) {
-		NodeMainExecutor nodeMainExecutor = DefaultNodeMainExecutor.newDefault();
+    public static void main(String[] args) {
+        NodeMainExecutor nodeMainExecutor = DefaultNodeMainExecutor.newDefault();
 		Talker publisherNode = new Talker();
 		String host = "127.0.0.1";
-	    URI masteruri = URI.create("http://"+host+":11311");
-	    NodeConfiguration pubNodeConfiguration = NodeConfiguration.newPublic(host, masteruri);
-	    nodeMainExecutor.execute(publisherNode, pubNodeConfiguration);   	
-	}
+		URI masteruri = URI.create("http://"+host+":11311");
+		NodeConfiguration pubNodeConfiguration = NodeConfiguration.newPublic(host, masteruri);
+		nodeMainExecutor.execute(publisherNode, pubNodeConfiguration);   	
+    }
 }
